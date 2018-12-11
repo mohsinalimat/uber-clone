972e31b1b423ec82589223a6c492074a0c137ff4
Emre Durukan <durukan.emre93@gmail.com>
2018-12-11 20:26:57 +0300

    Initial Commit

---
+++ Uber CloneTests/Uber_CloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  Uber_CloneTests.swift
+//  Uber CloneTests
+//
+//  Created by Emre Durukan on 11.12.2018.
+//  Copyright © 2018 Emre Durukan. All rights reserved.
+//
+
+import XCTest
+@testable import Uber_Clone
+
+class Uber_CloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
